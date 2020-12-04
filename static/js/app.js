89a3a4f2c21dfdd52ce72d989beaf4fc8ec2fc96
jaimechusid <jaimech@umich.edu>
2020-12-04 14:21:07 -0500

    finished app.js

--- static/js/app.js
+++ static/js/app.js
@@ -14,6 +14,18 @@ function buildTable(data) {
             cell.text(val);
         });
     });
-
 }
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    buildTable(filteredData);
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
 
